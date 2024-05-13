6379804d53a295a4dbd254ca4f18a2aefa3341cc
AB2326 <adam.boussatha@viacesi.fr>
2024-05-13 15:20:28 +0200

    xddd

--- demo_material_app2/lib/main.dart
+++ demo_material_app2/lib/main.dart
@@ -1,54 +1,58 @@
 import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const PractiseApp());
+  runApp(const MyApp());
 }
-
-class PractiseApp extends StatelessWidget {
-  const PractiseApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'First App',
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+        primarySwatch: Colors.blue,
+    ),
+    home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() =>
+      _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Ma première AppBar"),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Container(
-          height: 300,
-          width: 300,
-          color: Colors.lightBlueAccent,
-          child: const Center(
-            child: Text(
-              "Petit texte",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-              ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-          ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("J'ai cliqué sur le bouton");
-        },
-        child: const Icon(Icons.done),
-        backgroundColor: Colors.lightBlue, // Couleur du bouton flottant
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
