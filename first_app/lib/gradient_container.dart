1fe9ce3dfdebf7b46f25ab559600182d5caee7d2
AB2326 <adam.boussatha@viacesi.fr>
2024-05-13 11:52:08 +0200

    flutter

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 24, 25, 166),
+            Color.fromARGB(255, 24, 112, 166),
+          ])),
+      child: const Center(
+        child: Text("Hello World",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            )),
+      ),
+    );
+  }
+}
